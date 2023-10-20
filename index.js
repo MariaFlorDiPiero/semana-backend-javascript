49e94b4f897bb7921f6eeec537938fe11824cb5f
Maria Flor Di Piero <98435662+MariaFlorDiPiero@users.noreply.github.com>
2023-10-20 00:22:58 +0000

    Implement endpoint `Read all`

--- index.js
+++ index.js
@@ -9,4 +9,13 @@ app.get('/oi', function(req, res){
   res.send('Olá, mundo!')
 })
 
+//CRUD de lista de pokemons
+
+const items = ['Bulbassauro', 'Pikachu', 'Charmander', 'Charmeleon', 'Charizard']
+
+// RED All - Get/itens
+app.get('/items', function(req, res){
+  res.send(items)
+})
+
 app.listen(3000)
