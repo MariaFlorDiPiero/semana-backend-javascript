7347cf24a371a397dd268ecfe63cea26cc83cc46
Maria Flor Di Piero <98435662+MariaFlorDiPiero@users.noreply.github.com>
2023-10-20 21:47:16 +0000

    Create endpoint `post`

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+//Sinaliza para o express que o corpo das requisições
+//estará sempre em Json
+app.use(express.json())
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
@@ -16,6 +20,15 @@ const items = ['Bulbassauro', 'Pikachu', 'Charmander', 'Charmeleon', 'Charizard'
 // RED All - Get/itens
 app.get('/items', function(req, res){
   res.send(items)
+})
+
+// CREAT - POST - items
+
+app.post('/items', function(res,res){
+  console.log(req.body, typeof req.body)
+  const item = req.body.nome
+  console.log(item)
+  res.send('Create items')
 })
 
 app.listen(3000)
