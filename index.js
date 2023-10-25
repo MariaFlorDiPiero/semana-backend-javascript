d3b84aab703b20f1b895b3547655d794353fa04b
Maria Flor Di Piero <98435662+MariaFlorDiPiero@users.noreply.github.com>
2023-10-25 00:52:36 +0000

    Implement endpoint `Read by ID` `[GET]/items/:id`

--- index.js
+++ index.js
@@ -17,6 +17,21 @@ app.get('/oi', function(req, res){
 
 const items = ['Bulbassauro', 'Pikachu', 'Charmander', 'Charmeleon', 'Charizard']
 
+//READ by ID - [GET] /items/:id
+app.get('/items/:id', function (req, res){
+//Acessamos o parâmetro de rota ID
+//Subtraímos 1 para corrigir a questão do índice 
+// da lista que começa em 0
+  const id = req.params.id - 1
+
+//Acessamos o item na lista a partir do index
+  const item = items[id]
+
+
+//Exibimos o item obtido
+  res.send(item)
+})
+
 // RED All - Get/itens
 app.get('/items', function(req, res){
   res.send(items)
